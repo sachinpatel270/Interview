c670afe4859b275f7890f6fe61e1a3fddd3329b1
Samet Keserci <sammath@users.noreply.github.com>
2015-07-30 10:31:01 -0700

    Update Reccursion.java

--- Reccursion.java
+++ Reccursion.java
@@ -2,7 +2,7 @@
     /*
     Question 1: Knapsack with infinite number of items.
     Question 2: Knapsack without repetion of the items.
-    
+    Question 3: Least common subsequence if a given two sequence
     */
 
 
@@ -51,10 +51,33 @@
 		return max;
 	}
 	
-	    /*Question 2:
+      /*Question 2:
       Knapsack Problem with repetition, i.e there is only one item for each item.
       w= weight array, v= value array, tw =  weight capacity of knapsack. Aim is to maximize the
       total value in Knapsack.
      */
+     
+      /*Question 3:
+      Least Common subsequence of a given two sequence. ex: Inputs: s1 = "peacefully", s2 = "ecology". Output= "ecly"
+     */
+     
+     
+     
+     	public static String LCS(String s1, String s2) {
+		int s1L = s1.length();
+		int s2L = s2.length();
+		if (s1.length() == 0 || s2.length() == 0)
+			return "";
+
+		if (s1.charAt(0) == s2.charAt(0))
+			return s2.charAt(0) + LCS(s1.substring(1), s2.substring(1));
+		else {
+			String x = LCS(s1.substring(1), s2);
+			String y = LCS(s1, s2.substring(1));
+			return (x.length() > y.length()) ? x : y;
+
+		}
+
+	}
 	
 	

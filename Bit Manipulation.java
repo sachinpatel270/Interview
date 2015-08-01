18072ba2280dfcf96eaab256c90b5032fa44e048
Samet Keserci <sammath@users.noreply.github.com>
2015-07-31 22:23:00 -0700

    Update Bit Manipulation.java

--- Bit Manipulation.java
+++ Bit Manipulation.java
@@ -1,1 +1,19 @@
 
+/* Question 1:Given an array of numbers, all numbers are doubled except one, find that number
+   Question 2:Given an array of numbers, print the numbers that are repeated 3 times.
+*/
+
+
+    /* Question 1:
+    Given an array of numbers, all numbers are doubled except one, find that number
+    input: [1,4,2,3,5,4,3,1], output:5
+    */
+
+    public static int findSigleton(int[] ar) {
+       
+      int mask = 0;
+      for(int i = 0; i < ar.length; ++i) {
+         mask ^= ar[i];
+      }
+    return mask; 
+    }

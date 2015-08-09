86350516d0241175924754a0ac26e324f02163dd
Samet Keserci <sammath@users.noreply.github.com>
2015-08-09 14:13:00 -0700

    Update Hash.java

--- Hash.java
+++ Hash.java
@@ -1,6 +1,6 @@
 	/* 
 	Question 1: Taxicab Numbers. (x, such that x = a^3 + b^3 = c^3 + d^3, for different a,b,c,d)
-	Question 2: Ingeneral write a program to output unbiased outcome from biased coin
+
 	
 	
 	*/
@@ -33,26 +33,3 @@
 		}
 	}
 	
-	/* Question 2:
-	 Probability of getting 1 is 0.25, and getting 0 is 0.75. Write program to return 0 or 1 equally likely
-	 Ingeneral write a program to output unbiased outcome from a biased coin
-	*/
-	
-	public static int biasedCointoUnbiased1() {
-
-		int i = biasedCoin();
-		int j = biasedCoin();
-		int k = i ^ j;
-
-		return (k == 1) ? i : biasedCointoUnbiased1();
-	}
-
-	// biased coin for testing: 1 with probability .25, and 0 with probability .75
-	public static int biasedCoin() {
-
-		int[][] M = { { 1, 2 }, { 3, 4 } };
-		int i = (int) (2 * Math.random());
-		int j = (int) (2 * Math.random());
-
-		return (M[i][j] == 1) ? 1 : 0;
-	}

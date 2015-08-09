d8fb679450ed1ba1f41336847c43c84f47bf8149
Samet Keserci <sammath@users.noreply.github.com>
2015-08-09 14:12:20 -0700

    Update Bit Manipulation.java

--- Bit Manipulation.java
+++ Bit Manipulation.java
@@ -1,6 +1,7 @@
 
-    /* Question 1:Given an array of numbers, all numbers are doubled except one, find that number
-      Question 2:Given an array of numbers, print the numbers that are repeated 3 times.
+    /*Question 1: Given an array of numbers, all numbers are doubled except one, find that number.
+      Question 2: In general write a program to output unbiased outcome from biased coin.
+      Question 3: Given an array of numbers, print the numbers that are repeated 3 times.
     */
 
 
@@ -16,4 +17,27 @@
 			mask ^= ar[i];
 		}
 		return mask;
+	}
+
+	/* Question 2: 
+	 Probability of getting 1 is 0.25, and getting 0 is 0.75. Write program to return 0 or 1 equally likely
+	 In general write a program to output unbiased outcome from biased coin.
+	 */
+	public static int biasedCointoUnbiased1() {
+
+		int i = biasedCoin();
+		int j = biasedCoin();
+		int k = i ^ j;
+
+		return (k == 1) ? i : biasedCointoUnbiased1();
+	}
+
+	// biased coin for testing: 1 with probability .25, and 0 with probability .75
+	public static int biasedCoin() {
+
+		int[][] M = { { 1, 2 }, { 3, 4 } };
+		int i = (int) (2 * Math.random());
+		int j = (int) (2 * Math.random());
+
+		return (M[i][j] == 1) ? 1 : 0;
 	}

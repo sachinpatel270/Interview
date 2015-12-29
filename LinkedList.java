8be63b329d3f20175dc5024d690461cdf4c7eeba
sachinpatel270 <sachinpatel270@gmail.com>
2015-12-28 19:37:39 -0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -25,6 +25,12 @@
 				temp1.poll();
 				--diff;
 			}
+/*Methods for collections 
+
+Poll gets the head of the list and removes it from the list
+Peek gets the head/look at the top of stack But does not remove it
+PUSH adds the element in front of the list
+ADD adds the element at the specified position in the list*/
 
 			while (!temp1.peek().equals(temp2.peek())) {
 				temp1.poll();
